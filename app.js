decd1ca14a80075022cc2d96494033c303b66955
juandaza25 <juan.daza.mt@usa.edu.co>
2022-09-24 07:03:43 -0500

    Mi primera vers

---
+++ app.js
@@ -0,0 +1,28 @@
+//console.log("hola Juan Carlos");
+
+//console.log("Hello World");
+
+//var time = new Date();
+//console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+
+alert("esto es un mensaje en una ventana!");
+//document.write("La informacion se muestra dentro el desk");
+//document.write("<p>paragraph</p>");
+
+
+//otras maneras de crear los scripts es directamente en el codigo html
+
+//en el Head y en el body
+
+// en el head:
+//var time = new Date();
+//console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+
+
+// en el body
+//let d = new Date();
+//document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + //d.getMinutes() + ":" + d.getSeconds()
+
+
+//diferencias entre let, var, y const 
+//https://cybmeta.com/var-let-y-const-en-javascript
